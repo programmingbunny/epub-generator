1a7004cd32d01a20877d115a30e8823b9d2b7469
programmingbunny <bunnyprogramming@gmail.com>
2022-09-01 23:53:33 -0500

    added helper function for trimming image headers filenames

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,9 +1,17 @@
 package helpers
 
-import "crypto/rand"
+import (
+	"crypto/rand"
+	"strings"
+)
 
 // helper function for generating unique id for parent directory of epub file
 func NumberGen() string {
 	p, _ := rand.Prime(rand.Reader, 64)
 	return p.String()
+}
+
+func TrimImage(imgName string) string {
+	new := strings.TrimLeft(imgName, "../chapter-images/")
+	return new
 }
